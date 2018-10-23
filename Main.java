047f55dd63c3c3355065705520458375d9559ae9
Zac <tclu82@uw.edu>
2018-10-22 21:29:01 -0700

    Implement generateSubList method for dividing list into 4 sublists

--- Main.java
+++ Main.java
@@ -4,14 +4,31 @@ import java.util.Random;
 
 public class Main {
 
-    private static final int DATA_SIZE = 20;
+    private static final int DATA_SIZE = 40;
 
 
 
     public static void main(String... theArgs) {
         List<Integer> randomList = generateRandomNumber(DATA_SIZE);
-        System.out.println(randomList.size());
-        System.out.println(randomList);
+//        System.out.println(randomList);
+
+
+        try {
+            List<List<Integer>> subLists = new ArrayList<>();
+            for (int i = 0; i < DATA_SIZE; i += DATA_SIZE / 4) {
+                List<Integer> subList = generateSubList(randomList, i, i + DATA_SIZE / 4);
+                subLists.add(subList);
+            }
+            System.out.println(subLists);
+
+
+
+
+
+
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Please adjust DATA_SIZE can be divided by 4");
+        }
     }
 
     private static List<Integer> generateRandomNumber(int dataSize) {
@@ -27,6 +44,11 @@ public class Main {
             result.add(number);
         }
         return result;
+    }
+
+    private static List<Integer> generateSubList(List<Integer> list, int start, int end) {
+        List<Integer> subList = list.subList(start, end);
+        return subList;
     }
 
 }
