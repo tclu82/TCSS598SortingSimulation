8a2b76d59ffe267b0e5a495ffd67e0d1202546d6
Zac <tclu82@uw.edu>
2018-10-22 21:07:59 -0700

    Add generateRandomNumber method to generate random number

--- Main.java
+++ Main.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+
+    private static final int DATA_SIZE = 20;
+
+
+
+    public static void main(String... theArgs) {
+        List<Integer> randomList = generateRandomNumber(DATA_SIZE);
+        System.out.println(randomList.size());
+        System.out.println(randomList);
+    }
+
+    private static List<Integer> generateRandomNumber(int dataSize) {
+        Random rand = new Random();
+        List<Integer> list = new ArrayList<>();
+        for (int i=1; i<=dataSize; i++) {
+            list.add(i);
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int i=1; i<=dataSize; i++) {
+            int index = rand.nextInt(list.size());
+            int number = list.remove(index);
+            result.add(number);
+        }
+        return result;
+    }
+
 }
